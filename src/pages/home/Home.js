522874fbf30ba70fb7b8adb4c30d11013bb85e03
tranminh3008 <tranhuuminh3008@gmail.com>
2024-05-14 19:50:33 +0700

    base

---
+++ src/pages/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../Components/header/Header';
+import Footer from '../../Components/footer/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
